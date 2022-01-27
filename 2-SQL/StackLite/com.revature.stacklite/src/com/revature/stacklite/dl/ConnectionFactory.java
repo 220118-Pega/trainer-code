f9294aa3e8c293f2d5c00f5032a610ebaa88b371
Marielle Nolasco <marielletnolasco@gmail.com>
2022-01-27 09:01:38 -0800

    added getConnection method in connection factory

--- 2-SQL/StackLite/com.revature.stacklite/src/com/revature/stacklite/dl/ConnectionFactory.java
+++ 2-SQL/StackLite/com.revature.stacklite/src/com/revature/stacklite/dl/ConnectionFactory.java
@@ -1,16 +1,21 @@
 package com.revature.stacklite.dl;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
-	// eager loading - you create instance of connection factory as soon as the program starts up
+	// eager loading - you create instance of connection factory as soon as the
+	// program starts up
 	private static final ConnectionFactory connectionFactory = new ConnectionFactory();
 	// hold db config stuff
 	private Properties prop = new Properties();
-	
+
 	// force load postgresql driver
-	// static members of class get loaded into memory at the start of program runtime
+	// static members of class get loaded into memory at the start of program
+	// runtime
 	// static blocks get run at the start
 	static {
 		try {
@@ -30,12 +35,26 @@ public class ConnectionFactory {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// as well as the getInstance method
-	public static ConnectionFactory getInstance()
-	{
-		//lazy loading - create instance of connection factory when you call it 
-		//if(connectionFactory == null) connectionFactory = new ConnectionFactory();
+	public static ConnectionFactory getInstance() {
+		// lazy loading - create instance of connection factory when you call it
+		// if(connectionFactory == null) connectionFactory = new ConnectionFactory();
 		return connectionFactory;
+	}
+
+	// factories are characterized by some method that contains logic for object
+	// creation
+	public Connection getConnection() {
+		Connection conn = null;
+		// trying to get connection using db creds
+		try {
+			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+					prop.getProperty("password"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+
 	}
 }
