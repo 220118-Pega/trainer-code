71592e92428c1d0a1ab79c25387b3473d2dd9683
Marielle Nolasco <marielletnolasco@gmail.com>
2022-02-02 09:47:37 -0800

    Hero REST

--- 3-Javalin/TourOfHeroes/tourofheroes/src/main/java/com/revature/tourofheroes/HelloWorld.java
+++ 3-Javalin/TourOfHeroes/tourofheroes/src/main/java/com/revature/tourofheroes/HelloWorld.java
@@ -1,21 +1,29 @@
 
 package com.revature.tourofheroes;
 
-
 import com.revature.tourofheroes.models.Hero;
 import com.revature.tourofheroes.storage.HeroDAO;
 
 import io.javalin.Javalin;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-//    Javalin app = Javalin.create().start(7000);
-//    app.get("/", ctx -> ctx.result("Hello World"));
-    
-    HeroDAO heroDAO = new HeroDAO();
-    for(Hero hero:heroDAO.getAllHeroes())
-    {
-    	System.out.println(hero);
-    }
-  }
+	public static void main(String[] args) {
+		HeroDAO heroDAO = new HeroDAO();
+		Javalin app = Javalin.create().start(7000);
+		app.get("/", ctx -> ctx.result("Hello World"));
+		// technically to set up an endpoint you just need two things
+		// 1: String path
+		// 2. Action, when you get to this endpoint what is your code doing
+		// method params: String endPoint, Handler (what are you doing at the endpoint)
+		// Handler is a fcnal interface, contains only one method, you pass in fcns as
+		// implementations of this interface
+		app.get("/heroes", ctx -> ctx.jsonStream(heroDAO.getAllHeroes()));
+		app.get("/heroes/{hero_id}", ctx -> {
+			String stringId = ctx.pathParam("hero_id");
+			int id = Integer.parseInt(stringId);
+			Hero heroById = heroDAO.getHeroById(id);
+			ctx.jsonStream(heroById);
+		});
+		app.post("/heroes", ctx -> heroDAO.addHero(ctx.bodyStreamAsClass(Hero.class)));
+	}
 }
--- 3-Javalin/TourOfHeroes/tourofheroes/src/main/java/com/revature/tourofheroes/storage/HeroDAO.java
+++ 3-Javalin/TourOfHeroes/tourofheroes/src/main/java/com/revature/tourofheroes/storage/HeroDAO.java
@@ -13,26 +13,51 @@ import com.revature.tourofheroes.models.HeroType;
 public class HeroDAO {
 	public List<Hero> getAllHeroes() {
 		List<Hero> allHeroes = new ArrayList<Hero>();
-		try (Connection conn = ConnectionFactory.getInstance().getConnection())
-		{
+		try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
 			String query = "select * from heroes";
 			PreparedStatement pstmt = conn.prepareStatement(query);
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				allHeroes.add(
-						new Hero(
-								rs.getInt("id"),
-								rs.getString("alias"),
-								rs.getString("real_name"),
-								rs.getString("power"),
-								HeroType.valueOf(rs.getString("hero_type"))
-								)
-						);
+			while (rs.next()) {
+				allHeroes.add(new Hero(rs.getInt("id"), rs.getString("alias"), rs.getString("real_name"),
+						rs.getString("power"), HeroType.valueOf(rs.getString("hero_type"))));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return allHeroes;
+	}
+
+	public Hero getHeroById(int id) {
+		try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+			String query = "Select * from heroes where id = ?";
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return new Hero(rs.getInt("id"), rs.getString("alias"), rs.getString("real_name"),
+						rs.getString("power"), HeroType.valueOf(rs.getString("hero_type")));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void addHero(Hero newHero) {
+		try(Connection conn=ConnectionFactory.getInstance().getConnection())
+		{
+			String query  = "insert into heroes (alias, real_name, power, hero_type) values (?,?,?,CAST(? as heroType))";
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, newHero.getAlias());
+			pstmt.setString(2, newHero.getRealName());
+			pstmt.setString(3, newHero.getPower());
+			pstmt.setString(4, newHero.getType().toString());
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
